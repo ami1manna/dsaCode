178af7d0b3c63adef73b0f654593a5ff6d3265b1
Amit_manna <91798995+ami1manna@users.noreply.github.com>
2023-12-14 20:16:47 +0530

    Update quicksort.c

--- sorting/quicksort.c
+++ sorting/quicksort.c
@@ -1,70 +1,61 @@
 #include <stdio.h>
-#include <conio.h>
-#define size 100
-int partition(int a[], int beg, int end);
-void quick_sort(int a[], int beg, int end);
-void main()
-{
-int arr[size], i, n;
-printf("\n Enter the number of elements in the array: ");
-scanf("%d", &n);
-printf("\n Enter the elements of the array: ");
-for(i=0;i<n;i++)
-{
-scanf("%d", &arr[i]);
-}
-quick_sort(arr, 0, n-1);
-printf("\n The sorted array is: \n");
-for(i=0;i<n;i++)
-printf(" %d\t", arr[i]);
-getch();
-}
-int partition(int a[], int beg, int end)
-{
-int left, right, temp, loc, flag;
-loc = left = beg;
-right = end;
-flag = 0;
-while(flag != 1)
-{
-while((a[loc] <= a[right]) && (loc!=right))
-right--;
-if(loc==right)
-flag =1;
-else if(a[loc]>a[right])
-{
-temp = a[loc];
-a[loc] = a[right];
-a[right] = temp;
-loc = right;
-}
-if(flag!=1)
-{
 
-while((a[loc] >= a[left]) && (loc!=left))
-left++;
-if(loc==left)
-flag =1;
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
-else if(a[loc] <a[left])
-{
-temp = a[loc];
-a[loc] = a[left];
-a[left] = temp;
-loc = left;
+int Partition(int a[], int l, int h) {
+    int pivot = a[l];
+    int i = l;
+    int j = h;
 
+    do {
+        do {
+            i++;
+        } while (a[i] <= pivot);
+
+        do {
+            j--;
+        } while (a[j] > pivot);
+
+        if (i < j) {
+            swap(&a[i], &a[j]);
+        }
+    } while (i < j);
+
+    swap(&a[l], &a[j]);
+    return j;
 }
+
+void QuickSort(int a[], int l, int h) {
+    int j;
+    if (l < h) {
+        j = Partition(a, l, h);
+        QuickSort(a, l, j);
+        QuickSort(a, j + 1, h);
+    }
 }
-}
-return loc;
-}
-void quick_sort(int a[], int beg, int end)
-{
-int loc;
-if(beg<end)
-{
-loc = partition(a, beg, end);
-quick_sort(a, beg, loc-1);
-quick_sort(a, loc+1, end);
-}
+
+int main() {
+    int a[1000];
+    int n;
+
+    printf("Enter number of components: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter element: ");
+        scanf("%d", &a[i]);
+    }
+
+    QuickSort(a, 0, n);
+
+    printf("After sorting: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
 }
