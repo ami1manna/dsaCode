4b6997643338e16b38391682961162d97a929fb4
Amit_manna <91798995+ami1manna@users.noreply.github.com>
2024-01-06 18:48:39 +0530

    Update QueueusingLinked.c

--- queue/QueueusingLinked.c
+++ queue/QueueusingLinked.c
@@ -1,138 +1,85 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h>
 
-//Node structure for  Queue node
-struct Node{
-int data;
-struct Node* next;
-
+// Structure to create a node with data and the next pointer
+struct node {
+    int data;
+    struct node * next;
 };
-struct Queue{
-struct Node *front ;
-struct Node *rear ;
-};
-struct Queue *q ;
-//create node function 
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
+struct node * front = NULL;
+struct node * rear = NULL;
 
-//create Queue function code onnce 
- void createQueue(struct Queue *queue){
-
-queue->rear = NULL;
-queue->front= NULL;
-
-}
-
-//insert into Queue function code
-struct Queue* enqeue(struct Queue *queue, int data){
-    //new node
-      struct Node *newNode=  createNode(data);
-    //if q empty
-    if(queue->front ==NULL ){
-        queue->front = queue->rear = newNode;
+// Enqueue() operation on a queue
+void enqueue(int value) {
+    struct node * ptr;
+    ptr = (struct node * ) malloc(sizeof(struct node));
+    ptr -> data = value;
+    ptr -> next = NULL;
+    if ((front == NULL) && (rear == NULL)) {
+        front = rear = ptr;
+    } else {
+        rear -> next = ptr;
+        rear = ptr;
     }
-    else {
-       queue->rear->next = newNode;
-       queue->rear = newNode;
-       queue->rear->next =NULL;
+    printf("Node is Inserted\n\n");
+}
 
+// Dequeue() operation on a queue
+int dequeue() {
+    if (front == NULL) {
+        printf("\nUnderflow\n");
+        return -1;
+    } else {
+        struct node * temp = front;
+        int temp_data = front -> data;
+        front = front -> next;
+        free(temp);
+        return temp_data;
     }
-return queue;
-
 }
 
-struct Queue *dequeue(struct Queue *queue){
-if(queue->front == NULL){
-    printf("Queue is empty\n");
-    
-
-}
-else{
-// store cuurent top node in temp var
-struct Node *temp = queue->front;
-//store date of top node
-int data = queue->front->data;
-// point next node 
-queue->front = queue->front->next ;
-//free the topnode
-free(temp);
-
-}
-return queue;
-}
-
-
-struct Queue* display(struct Queue *queue){
-    struct Node *ptr  = queue->front;
-    if(ptr==NULL){
-        printf("Underflow\n");
+// Display all elements of the queue
+void display() {
+    struct node * temp;
+    if ((front == NULL) && (rear == NULL)) {
+        printf("\nQueue is Empty\n");
+    } else {
+        printf("The queue is \n");
+        temp = front;
+        while (temp) {
+            printf("%d--->", temp -> data);
+            temp = temp -> next;
+        }
+        printf("NULL\n\n");
     }
-    else{
-        while(ptr!=NULL){
-            printf("data: %d\n", ptr->data);
-            ptr = ptr->next;  // Move to the next node in the loop
+}
+
+int main() {
+    int choice, value;
+    printf("\nImplementation of Queue using Linked List\n");
+    while (choice != 4) {
+        printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n");
+        printf("\nEnter your choice : ");
+        scanf("%d", & choice);
+        switch (choice) {
+            case 1:
+                printf("\nEnter the value to insert: ");
+                scanf("%d", & value);
+                enqueue(value);
+                break;
+            case 2:
+                printf("Popped element is :%d\n", dequeue());
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+                break;
+            default:
+                printf("\nWrong Choice\n");
         }
     }
-    printf("\n");
-    return queue;
-}
-
-
-//code for peek
-int peek(struct Queue *q)
-{
-if(q->front==NULL)
-{
-printf("\n QUEUE IS EMPTY");
-return -1;
-}
-else
-return q->front->data;
-}
-// MAIN MODE
-int main()
-{
-int val, option;
-//intialize queue rear and front to Null
-q = (struct Queue *)malloc(sizeof(struct Queue));
-createQueue(q);
-
-do
-{
-printf("\n *****MAIN MENU*****");
-printf("\n 1. INSERT");
-printf("\n 2. DELETE");
-printf("\n 3. PEEK");
-printf("\n 4. DISPLAY");
-printf("\n 5. EXIT");
-printf("\n Enter your option : ");
-scanf("%d", &option);
-switch(option)
-{
-case 1:
-printf("\n Enter the number to insert in the queue:");
-scanf("%d", &val);
-q = enqeue(q,val);
-break;
-case 2:
-q = dequeue(q);
-break;
-case 3:
-val = peek(q);
-if(val!= -1)
-printf("\n The value at front of queue is : %d", val);
-break;
-case 4:
-q = display(q);
-break;
-}
-}while(option != 5);
-
-return 0;
+    return 0;
 }
